e12aaaf53a2bdf58c52a97574788b53ad1bfdabb
kafleg <merohappylife@gmail.com>
2020-04-12 22:33:05 +0545

    remove comments of js file

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,38 +1,23 @@
-
 /**
  *  custom js for the theme
  * 
  */
-
 jQuery(document).ready(function ($) {
-	// Initialize gototop for carousel
-		if ( $('#toTop').length > 0 ) {
-		    // Hide the toTop button when the page loads.
-		    $("#toTop").css("display", "none");
-
-		      // This function runs every time the user scrolls the page.
-		      $(window).scroll(function(){
-		        // Check weather the user has scrolled down (if "scrollTop()"" is more than 0)
-		        if($(window).scrollTop() > 0){
-		          // If it's more than or equal to 0, show the toTop button.
-		          $("#toTop").fadeIn("slow");
-		      }
-		      else {
-		          // If it's less than 0 (at the top), hide the toTop button.
-		          $("#toTop").fadeOut("slow");
-		      }
-		  	});
-
-			// When the user clicks the toTop button, we want the page to scroll to the top.
-			jQuery("#toTop").click(function(event){
-
-				// Disable the default behaviour when a user clicks an empty anchor link.
-				// (The page jumps to the top instead of // animating)
-				event.preventDefault();
-				// Animate the scrolling motion.
-				jQuery("html, body").animate({
-					scrollTop:0
-				},"slow");
-			});
-	  	}
+	if ( $('#toTop').length > 0 ) {
+	    $("#toTop").css("display", "none");
+	      $(window).scroll(function(){
+	        if($(window).scrollTop() > 0){
+	          $("#toTop").fadeIn("slow");
+	      }
+	      else {
+	          $("#toTop").fadeOut("slow");
+	      }
+	  	});
+		jQuery("#toTop").click(function(event){
+			event.preventDefault();
+			jQuery("html, body").animate({
+				scrollTop:0
+			},"slow");
+		});
+  	}
 } );
